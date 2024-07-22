f62040e9af48de76cdd284f14fca3dfdd33fd85f
CongNC <ngchicong246@gmail.com>
2024-07-22 22:34:43 +0700

    add Discount gRPC

---
+++ Services/Discount/DisCount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,17 @@
+﻿using DisCount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DisCount.Grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+    public DiscountContext(DbContextOptions<DiscountContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "Iphone X", Description = "Iphone X", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung S23", Description = "Samsung S23", Amount = 100 });
+    }
+}
